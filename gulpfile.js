57e7d61c6ca168e3c19017887ce6577bee99772e
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2017-10-14 11:07:05 -0400

    Inicio del proyecto

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp'),
+      browserSync = require('browser-sync').create(),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      pug = require('gulp-pug');
+
+gulp.task('default', () =>{
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch('./*.html').on('change',browserSync.reload);
+  gulp.watch('./css/*.css').on('change',browserSync.reload);
+  gulp.watch('./js/*.js').on('change',browserSync.reload);
+  gulp.watch('./sass/*.sass', ['sass']);//.on('change', )
+  gulp.watch('./jade/*.pug', ['pug']);//.on('change', )
+})
+gulp.task('sass', () =>{
+  gulp.src('./sass/*.sass')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      versions: ['last 2 browers']
+    }))
+    .pipe(gulp.dest('./css'))
+})
+gulp.task('pug', ()=>{
+  gulp.src('./jade/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./'))
+})
