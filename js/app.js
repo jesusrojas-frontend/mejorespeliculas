81ceec769a24d252377fee51f207d0262a777330
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2017-10-21 15:50:44 -0400

    se termino el responsive

--- js/app.js
+++ js/app.js
@@ -28,10 +28,24 @@ var bLazy = new Blazy({
 var $categorias = document.getElementById('categorias');
 var $categoriaMenu = document.getElementById('categoria-menu');
 var $iconoMenu = document.getElementById('icono-menu');
+var $boton = document.getElementById('boton');
+var $contenedor = document.getElementById('contenedor');
+var consulta = window.matchMedia('(max-width: 500px)');
+consulta.addListener(mediaQuery)
 $categorias.addEventListener("click", contraer);
-
+$boton.addEventListener("click", ocultar);
+function ocultar() {
+  $contenedor.classList.toggle('active')
+}
 function contraer() {
   $categoriaMenu.classList.toggle('active');
   $categoriaMenu.classList.toggle('ocultar');
   $iconoMenu.classList.toggle('rotar');
 }
+function mediaQuery() {
+  if(consulta.matches){
+    $categoriaMenu.classList.remove('active');
+    console.log("Hola")
+  }
+}
+mediaQuery();
